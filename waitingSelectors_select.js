0e3fd069cabdcc652d4c412fe85a1b2d562bed55
Denis <litecarma@gmail.com>
2022-09-12 16:59:56 +0700

    chore(): add variant with variable for validate functior

--- waitingSelectors_select.js
+++ waitingSelectors_select.js
@@ -1,43 +1,58 @@
 function ValidateSelector(sel) {
-  var valid = /^$|\s*>MATCH>|^\s*>CSS>|^\s*>XPATH>|^\s*>img:\d+:(?:scroll|no scroll)>/i.test(sel.replace(/['"]+/g,''))
+  var valid =
+    /(?:(?:\[\[[\S]+\]\])|(?:^$|\s*>MATCH>|^\s*>CSS>|^\s*>XPATH>|^\s*>img:\d+:(?:scroll|no scroll)>))/.test(
+      sel.replace(/['"]+/g, '')
+    );
   if (!valid) {
-    Invalid(tr("not valid selector: ") + sel);
+    Invalid(tr('not valid selector: ') + sel);
   }
-  return valid
+  return valid;
 }
 
-var timeout = GetInputConstructorValue("timeout", loader);
-if(timeout["original"].length <= 0){
-	Invalid(tr("timeout") + " " + tr("is empty"));
-    return;
-};
+var timeout = GetInputConstructorValue('timeout', loader);
+if (timeout['original'].length <= 0) {
+  Invalid(tr('timeout') + ' ' + tr('is empty'));
+  return;
+}
 
 var codeVariables = {
-  timeout: timeout["updated"],
-  "allResults": this.$el.find("#allResults").val().toUpperCase()
-}
+  timeout: timeout['updated'],
+  allResults: this.$el.find('#allResults').val().toUpperCase(),
+};
 
-var selectors = {}
-var countInputs =  document.querySelectorAll('.input_selector_string[id^="sel"]').length
+var selectors = {};
+var countInputs = document.querySelectorAll(
+  '.input_selector_string[id^="sel"]'
+).length;
 for (i = 1; i <= countInputs; i++) {
-  var id = 'sel' + i
-  var sel = GetInputConstructorValue(id, loader)
-  var isValid = ValidateSelector(sel["original"])
+  var id = 'sel' + i;
+  var sel = GetInputConstructorValue(id, loader);
+  var isValid = ValidateSelector(sel['original']);
   if (!isValid) {
-    return
+    return;
   }
   selectors[id] = {
-    sel: sel["updated"],
+    sel: sel['updated'],
     visible: $('#' + id + 'Visible').is(':checked'),
-    saveVariable: this.$el.find("#" + id + 'Save').val().toUpperCase()
-  }
+    saveVariable: this.$el
+      .find('#' + id + 'Save')
+      .val()
+      .toUpperCase(),
+  };
 }
 
-codeVariables.selectors = JSON.stringify(selectors)
-try{
-  var code = loader.GetAdditionalData() + _.template($("#waitingSelectors_code").html())(codeVariables);
-  code = Normalize(code,0);
-  BrowserAutomationStudio_Append("", BrowserAutomationStudio_SaveControls() + code, action, DisableIfAdd);
-}catch(e){
-  console.log(e)
+codeVariables.selectors = JSON.stringify(selectors);
+try {
+  var code =
+    loader.GetAdditionalData() +
+    _.template($('#waitingSelectors_code').html())(codeVariables);
+  code = Normalize(code, 0);
+  BrowserAutomationStudio_Append(
+    '',
+    BrowserAutomationStudio_SaveControls() + code,
+    action,
+    DisableIfAdd
+  );
+} catch (e) {
+  console.log(e);
 }
